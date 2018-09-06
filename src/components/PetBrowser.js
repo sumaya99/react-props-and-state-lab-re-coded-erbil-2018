ebda23277370c72de259cd894e23252d78eb0b68
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-09-06 05:30:09 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-import Pet from './Pet';
-
-class PetBrowser extends React.Component {
-  render() {
-    return (
-      <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+const Pet = ({ pet: { id, name, type, gender, age, weight, isAdopted }, onAdoptPet }) => (
+  <div className="card">
+    <div className="content">
+      <a className="header">
+        {name} {gender === 'female' ? '♀' : '♂'}
+      </a>
+      <div className="meta">
+        <span className="date">{type}</span>
       </div>
-    );
-  }
-}
+      <div className="description">
+        <p>Age: {age}</p>
+        <p>Weight: {weight}</p>
+      </div>
+    </div>
+    <div className="extra content">
+      {isAdopted ? (
+        <button className="ui disabled button">Already adopted</button>
+      ) : (
+        <button onClick={() => onAdoptPet(id)} className="ui primary button">
+          Adopt pet
+        </button>
+      )}
+    </div>
+  </div>
+);
 
-export default PetBrowser;
+export default Pet;
