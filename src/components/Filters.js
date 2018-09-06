2b55209665e89dc7669e7c7c80f008c4fb147435
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-09-06 05:29:49 +0000

    automatically backed up by learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,29 +1,23 @@
 import React from 'react';
 
-class Filters extends React.Component {
-  constructor() {
-    super();
-  }
+const Filters = ({ onChangeType, onFindPetsClick }) => (
+  <div className="ui form">
+    <h3>Animal type</h3>
+    <div className="field">
+      <select name="type" id="type" onChange={onChangeType}>
+        <option value="all">All</option>
+        <option value="cat">Cats</option>
+        <option value="dog">Dogs</option>
+        <option value="micropig">Micropigs</option>
+      </select>
+    </div>
 
-  render() {
-    return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
-
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
-      </div>
-    );
-  }
-}
+    <div className="field">
+      <button onClick={onFindPetsClick} className="ui secondary button">
+        Find pets
+      </button>
+    </div>
+  </div>
+);
 
 export default Filters;
