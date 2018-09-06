cc5ec7a463501a44afb96762aef2e4587b9fc127
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-09-06 05:30:51 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,29 +1,11 @@
 import React from 'react';
 
-const Pet = ({ pet: { id, name, type, gender, age, weight, isAdopted }, onAdoptPet }) => (
-  <div className="card">
-    <div className="content">
-      <a className="header">
-        {name} {gender === 'female' ? '♀' : '♂'}
-      </a>
-      <div className="meta">
-        <span className="date">{type}</span>
-      </div>
-      <div className="description">
-        <p>Age: {age}</p>
-        <p>Weight: {weight}</p>
-      </div>
-    </div>
-    <div className="extra content">
-      {isAdopted ? (
-        <button className="ui disabled button">Already adopted</button>
-      ) : (
-        <button onClick={() => onAdoptPet(id)} className="ui primary button">
-          Adopt pet
-        </button>
-      )}
-    </div>
-  </div>
-);
+import Pet from './Pet';
 
-export default Pet;
+const PetBrowser = ({ pets, onAdoptPet }) => {
+  const petCards = pets.map(pet => <Pet pet={pet} key={pet.id} onAdoptPet={onAdoptPet} />);
+
+  return <div className="ui cards">{petCards}</div>;
+};
+
+export default PetBrowser;
